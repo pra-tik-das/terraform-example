a755f25e5ac62c837c20b9d243527e7a373628a2
pra-tik-das <pratik.x.das@gmail.com>
2020-11-09 13:30:04 -0800

    updated vpc properties

--- vpc_igw.tf
+++ vpc_igw.tf
@@ -1,10 +1,16 @@
 resource "aws_vpc" "sage_payroll" {
-    cidr_block              = "${var.vpc_cidr}"
-    instance_tenancy        = "${var.instance_tenancy}"
-    enable_dns_hostnames    = true
-    enable_dns_support      = true
+    cidr_block                = "${var.vpc_cidr}"
+    instance_tenancy          = "${var.instance_tenancy}"
+    enable_dns_hostnames      = true
+    enable_dns_support        = true
+    enable_nat_gateway        = true 
+    enable_vpn_gateway        = true 
+    enable_classiclink        = true 
+
+    enable_s3_endpoint        = true 
+    enable_dynamodb_endpoint  = true 
     tags = {
-        Name                = "${var.vpc_name}"
+        Name                  = "${var.vpc_name}"
     }
 }
 
